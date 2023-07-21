61fd91101c48eb3a734cc3c410310ced0cdc701c
Robby6Strings <rausten93@gmail.com>
2023-07-22 07:46:38 +1200

    clean up server socket logic

--- src/server/socket.ts
+++ src/server/socket.ts
@@ -5,14 +5,10 @@ import { ErrorMessage, MessageType } from "../shared/message"
 import { GameService } from "./services/gameService"
 import { PlayerService } from "./services/playerService"
 import { ServerGame } from "./game/serverGame"
-type ConnGameRef = {
-  __game?: ServerGame
-}
-export const socketHandler = (
-  conn: SocketStream & ConnGameRef,
-  _req: FastifyRequest
-) => {
+
+export const socketHandler = (conn: SocketStream, _req: FastifyRequest) => {
   conn.setEncoding("utf8")
+  let game: ServerGame | undefined = undefined
   const player = PlayerService.newPlayer(conn)
   conn.socket.send(
     JSON.stringify({ type: MessageType.auth, playerId: player.id })
@@ -25,19 +21,21 @@ export const socketHandler = (
       case MessageType.ping:
         conn.socket.send(JSON.stringify({ type: MessageType.ping }))
         break
+
       case MessageType.newGame:
-        conn.__game = GameService.newGame()
-        conn.__game.addPlayer(player)
+        game = GameService.newGame()
+        game.addPlayer(player)
         conn.socket.send(
           JSON.stringify({
             type: MessageType.gameState,
-            gameState: conn.__game.serialize(),
+            gameState: game.serialize(),
           })
         )
         break
+
       case MessageType.joinGame:
         console.log("join game", data.gameId)
-        const game = GameService.getGame(data.gameId)
+        game = GameService.getGame(data.gameId)
         if (!game) {
           conn.socket.send(
             JSON.stringify({
@@ -47,21 +45,21 @@ export const socketHandler = (
           )
           return
         }
-        conn.__game = game
         game.addPlayer(player)
         conn.socket.send(
           JSON.stringify({
             type: MessageType.gameState,
-            gameState: conn.__game.serialize(),
+            gameState: game.serialize(),
           })
         )
-        conn.__game.broadcastPlayer(player)
+        game.broadcastPlayer(player)
         break
 
       case MessageType.action:
-        conn.__game!.handleAction(data.action)
-        conn.__game!.broadcastAction(data.action)
+        game?.handleAction(data.action)
+        game?.broadcastAction(data.action)
       default:
+        break
     }
   })
 }
